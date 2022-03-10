199be634cf3507051e5aafba4b96a6bc99eeb60e
momo <mouiad@kuwaitnet.dev>
2022-03-10 23:12:33 +0200

    [WIP]: deploy.

--- heartcare/heartcare/wsgi.py
+++ heartcare/heartcare/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medconsulting.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heartcare.settings')
 
 application = get_wsgi_application()
