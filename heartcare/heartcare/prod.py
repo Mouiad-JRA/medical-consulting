c2069fb33506e766b3cca63dca29bbf0bd4502e3
momo <mouiad@kuwaitnet.dev>
2022-03-12 19:17:43 +0200

    [fix]: fix .

--- heartcare/heartcare/prod.py
+++ heartcare/heartcare/prod.py
@@ -7,4 +7,4 @@ DEBUG = False
 
 SECRET_KEY = '-+&=0+&@qy*mtcycbptxg8(_l!5!o7_z-*-&2yz2j#l#4tm04g'
 
-ALLOWED_HOSTS = ['medical-test1.herokuapp.com']
+ALLOWED_HOSTS = ['medical-consulting.herokuapp.com']
