838ca4b515940223e197ee1fdf6eb91b383996f3
momo <mouiad.alilo@gmail.com>
2022-04-17 18:20:55 +0300

    fix: edit the languages.

--- heartcare/heartcare/urls.py
+++ heartcare/heartcare/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -8,13 +8,19 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 admin.site.enable_nav_sidebar = False
 
-urlpatterns = i18n_patterns(
+
+urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('hospital.urls')),
     path('appointment/', include('appointment.urls')),
     path('captcha/', include('captcha.urls')),
     path("mws/", TemplateView.as_view(template_name="mws.html"), name="mws"),
+
     prefix_default_language=False
 )
 
