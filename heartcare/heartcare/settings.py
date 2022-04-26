f10b15b5080238d78d8f28ba378d9b3690544f49
momo <mouiad.alilo@gmail.com>
2022-04-26 21:22:29 +0300

    fix: fix send email.

--- heartcare/heartcare/settings.py
+++ heartcare/heartcare/settings.py
@@ -154,7 +154,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mouiad.alilo@gmail.com'
-EMAIL_HOST_PASSWORD = 'momoj2mfhana12'
+EMAIL_HOST_PASSWORD = 'qaz123zaq321!'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
