fde1c31cc192cc32639ed540dee249aae2558d70
momo <mouiad.alilo@gmail.com>
2022-04-26 20:48:49 +0300

    fix: fix send email.

--- heartcare/heartcare/settings.py
+++ heartcare/heartcare/settings.py
@@ -153,7 +153,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kncareerskwait@gmail.com'
-EMAIL_HOST_PASSWORD = 'qaz123zaq321!'
+EMAIL_HOST_USER = 'mouiad.alilo@gmail.com'
+EMAIL_HOST_PASSWORD = 'momohanaj2mf'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
